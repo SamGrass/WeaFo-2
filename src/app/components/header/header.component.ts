c12dce26b1317cfb63bd0b734438fd4cb9b04cdf
Samuele Grassetti <samuele.grassetti@overit.it>
2025-06-05 16:08:48 +0200

    Add routing and initialize basic components

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  templateUrl: './header.component.html',
+  imports: [
+    RouterLink
+  ],
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+
+}
---
+++ src/app/components/weather-table/weather-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-table',
+  standalone: true,
+  templateUrl: './weather-table.component.html',
+  styleUrls: ['./weather-table.component.scss']
+})
+export class WeatherTableComponent {
+
+}
