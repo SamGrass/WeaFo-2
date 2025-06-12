98c625bcfc6ac55509b7e80632dd94c519e22469
Samuele Grassetti <samuele.grassetti@overit.it>
2025-06-12 14:21:11 +0200

    Import class transform: reflect metadata

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {RouterOutlet} from "@angular/router";
 import {FooterComponent} from "./components/footer/footer.component";
 import {HeaderComponent} from "./components/header/header.component";
+import 'reflect-metadata';
 
 @Component({
   selector: 'app-root',
