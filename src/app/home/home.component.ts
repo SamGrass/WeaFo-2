4c3fcce0b75c778c9ac23b6db7798bb43641e2c9
Samuele Grassetti <samuele.grassetti@overit.it>
2025-06-16 17:47:09 +0200

    Refactor home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,34 +1,26 @@
 import {Component, inject, OnInit} from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule} from '@angular/common';
 import {WeatherService} from "../services/weather.service";
 import {ApiForecastResponse} from "../models/api-forecast-response.model";
+import {WeatherTableComponent} from "../components/weather-table/weather-table.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, WeatherTableComponent],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-   cities = ["London", "Tokyo", "New York", "Paris", "Milan", "Sydney", "Cairo", "Rio de Janeiro", "Toronto", "Berlin"];
-   forecastDataList: ApiForecastResponse[] | [] = [];
-   private readonly wService = inject(WeatherService);
+  protected citiesForecastList: ApiForecastResponse[] = [];
+  private readonly cities = ["London", "Tokyo", "New York", "Paris", "Milan", "Sydney", "Cairo", "Rio de Janeiro", "Toronto", "Berlin"];
+  private readonly weatherService = inject(WeatherService);
 
-  async ngOnInit() {
-      {
-      try {
-        const promises = this.cities.map(item => {
-          return this.wService.getForecastFromCityName(item)
-        })
-        this.forecastDataList = await Promise.all(promises)
-        console.log(this.forecastDataList[0].dailyData)
-      } catch (err) {
-        console.log(err);
-        throw err;
-      } finally {
-        console.log(this.forecastDataList);
-      }
-    }
+  ngOnInit() {
+    this.init().then();
+  }
+
+  private async init() {
+    const promises = this.cities.map(item => this.weatherService.getForecastFromCityName(item))
+    this.citiesForecastList = await Promise.all(promises)
   }
 }
