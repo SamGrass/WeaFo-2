d4c412c8e8715b8884904a79bfa9928b9dbdd0e1
Samuele Grassetti <samuele.grassetti@overit.it>
2025-06-19 11:58:39 +0200

    Refactor home and weather table to use daily forecast methods

--- src/app/components/weather-table/weather-table.component.ts
+++ src/app/components/weather-table/weather-table.component.ts
@@ -1,7 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {CommonModule} from "@angular/common";
-import {ApiForecastResponse} from "../../models/api-forecast-response.model";
 import {RouterLink} from "@angular/router";
+import {ApiForecastResponse} from "../../models/api-forecast-response.model";
 
 @Component({
   selector: 'app-weather-table',
@@ -10,6 +10,5 @@ import {RouterLink} from "@angular/router";
   imports: [CommonModule, RouterLink]
 })
 export class WeatherTableComponent {
-  @Input() forecastList: ApiForecastResponse[] = [];
-  protected readonly iconBaseUrl= 'https://openweathermap.org/img/wn/'
+  @Input() citiesForecastList!: ApiForecastResponse[];
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {WeatherService} from "../services/weather.service";
+import {ForecastService} from "../services/forecast.service";
 import {ApiForecastResponse} from "../models/api-forecast-response.model";
 import {WeatherTableComponent} from "../components/weather-table/weather-table.component";
 
@@ -13,14 +13,14 @@ import {WeatherTableComponent} from "../components/weather-table/weather-table.c
 export class HomeComponent implements OnInit {
   protected citiesForecastList: ApiForecastResponse[] = [];
   private readonly cities = ["London", "Tokyo", "New York", "Paris", "Milan", "Sydney", "Cairo", "Rio de Janeiro", "Toronto", "Berlin"];
-  private readonly weatherService = inject(WeatherService);
+  private readonly forecastService = inject(ForecastService);
 
   ngOnInit() {
     this.init().then();
   }
 
   private async init() {
-    const promises = this.cities.map(item => this.weatherService.getForecastFromCityName(item))
+    const promises = this.cities.map(item => this.forecastService.getForecastFromCityName(item))
     this.citiesForecastList = await Promise.all(promises)
   }
 }
